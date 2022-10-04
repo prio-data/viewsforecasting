9ac915f799632943fa15f087c5521b6b19de75a5
malikarakh <88826060+malikarakh@users.noreply.github.com>
2022-10-04 11:48:01 +0200

    quick_back_up_of_work

---
+++ MonthlyUpdates/Data visualization/dummy_common_variables.py
@@ -0,0 +1,32 @@
+#wishlist of .py that can pull current and matching past runs
+#this will help pull data into the present results
+
+#At CM level first
+
+EndOfHistory = 508
+
+#current month
+dev_id_cm = 'Fatalities001'
+run_id_cm = dev_id_cm 
+prod_id_cm = '2022_04_t01'
+level_cm = 'cm'
+
+
+#comparison month
+dev_id_cm_old = 'Fatalities001'
+run_id_cm_old = dev_id_cm_old
+prod_id_cm_old = '2022_03_t01'
+
+
+#At PGM level
+
+#current run
+dev_id_pgm = 'Fatalities001'
+run_id_pgm = dev_id_pgm
+prod_id_pgm = '2022_04_t01'
+level_pgm = 'pgm'
+
+#comparison month
+dev_id_pgm_old = 'Fatalities001'
+run_id_pgm_old = dev_id_pgm_old
+prod_id_pgm_old = '2022_03_t01'
