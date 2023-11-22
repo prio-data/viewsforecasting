025e752b61b1f5b7aea7c8afe370d96604f2cb42
ekaterinakuzmina <rina.kuzmina@gmail.com>
2023-11-22 16:54:55 +0100

    Comment back the FixedFirstSplitRegression

--- SystemUpdates/ModelDefinitions.py
+++ SystemUpdates/ModelDefinitions.py
@@ -7,7 +7,7 @@ from sklearn.ensemble import GradientBoostingRegressor
 from xgboost import XGBRegressor, XGBRFRegressor
 from lightgbm import LGBMRegressor
 
-from Tools.ViewsEstimators import HurdleRegression
+from Tools.ViewsEstimators import HurdleRegression, FixedFirstSplitRegression
 
 
 def DefineEnsembleModels(level):
@@ -16,18 +16,18 @@ def DefineEnsembleModels(level):
     if level == 'cm':
         nj = 12
 
-#        model = {
-#            'modelname':        'fatalities003_baseline_ons',
-#            'algorithm':        FixedFirstSplitRegression(ones_name='LGBMClassifier', zeros_name='LGBMRegressor',onset_indicator = ''),
-#            'depvar':           'ln_ged_sb_dep',
-#            'data_train':       'baseline002',
-#            'queryset':         'fatalities002_baseline',
-#            'preprocessing':    'float_it',
-#            'level':            'cm',
-#            'description':      'Baseline model with a few conflict history features as well as log population, random forests regression model.',
-#            'long_description':  'A very simple model with only five data columns (each column representing one feature): The number of fatalities in the same country at $t-1$, three decay functions of time since there was at least five fatalities in a single month, for each of the UCDP conflict types -- state-based, one-sided, or non-state conflict -- and log population size (Hegre2020RP,Pettersson2021JPR).The features in the baseline are included in all the models described below. This ensures that all models in the ensemble provides at least moderately good predictions, while guaranteeing diversity in feature sets and modelling approaches.'
-#        }
-#        ModelList.append(model)
+        #model = {
+        #    'modelname':        'fatalities003_baseline_ons',
+        #    'algorithm':        FixedFirstSplitRegression(ones_name='LGBMClassifier', zeros_name='LGBMClassifier',onset_indicator = 'split_by'),
+        #    'depvar':           'ln_ged_sb_dep',
+        #    'data_train':       'baseline002',
+        #    'queryset':         'fatalities002_baseline',
+        #    'preprocessing':    'float_it',
+        #    'level':            'cm',
+        #    'description':      'Baseline model with a few conflict history features as well as log population, random forests regression model.',
+        #    'long_description':  'A very simple model with only five data columns (each column representing one feature): The number of fatalities in the same country at $t-1$, three decay functions of time since there was at least five fatalities in a single month, for each of the UCDP conflict types -- state-based, one-sided, or non-state conflict -- and log population size (Hegre2020RP,Pettersson2021JPR).The features in the baseline are included in all the models described below. This ensures that all models in the ensemble provides at least moderately good predictions, while guaranteeing diversity in feature sets and modelling approaches.'
+        #}
+        #ModelList.append(model)
         
         model = {
             'modelname':        'fatalities003_nl_baseline_rf',
